b864984c4fec0b1ee30e85785c1fab5f4d487e45
ClaireDavis <cpv26@cornell.edu>
2015-11-18 11:04:18 -0500

    adding aws

---
+++ Fax-Machine/Fax-Machine/constants.m
@@ -0,0 +1,16 @@
+//
+//  constants.m
+//  Fax-Machine
+//
+//  Created by Claire Davis on 11/18/15.
+//  Copyright © 2015 Flatiron-School. All rights reserved.
+//
+
+#import "constants.h"
+
+@implementation constants
+
+ NSString *const POOL_ID = @"us-east-1:d968edde-bfff-4ba5-b560-e7d8fb1996ba";
+
+
+@end

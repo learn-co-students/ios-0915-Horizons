1923a0018387b5de56ac92a203d3c61056d7937d
ClaireDavis <cpv26@cornell.edu>
2015-11-18 14:29:10 -0500

    adding download manager

--- Fax-Machine/Fax-Machine/ImageUploadViewController.m
+++ Fax-Machine/Fax-Machine/ImageUploadViewController.m
@@ -84,6 +84,9 @@
  *  @param sender UINavigation right bar Done button.
  */
 - (IBAction)finishedImageSelect:(id)sender {
+  [self dismissViewControllerAnimated:YES completion:^{
+    NSLog(@"done");
+  }];
   NSLog(@"done");
   UIImage *image = self.selectedImage;
   NSString *fileName = [[[NSProcessInfo processInfo] globallyUniqueString] stringByAppendingString:@".png"];
@@ -94,14 +97,6 @@
   NSLog(@"filepath %@", filePath);
 
   NSData * imageData = UIImagePNGRepresentation(image);
-  NSLog(@"imagedata %@", imageData);
-
-  
-
-  
-  
-  
-  
   
   [imageData writeToFile:filePath atomically:YES];
   

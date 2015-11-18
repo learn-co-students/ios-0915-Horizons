d587c3f3d3eaf2c0a099aa443ed759eeac15f9b4
kevinlin505 <kevinlin505@hotmail.com>
2015-11-18 10:34:12 -0500

    Removing connection testing code

--- Fax-Machine/Fax-Machine/AppDelegate.m
+++ Fax-Machine/Fax-Machine/AppDelegate.m
@@ -21,19 +21,21 @@
 
     //Setting up initial connection with the Parse app.
     [Parse setApplicationId:PARSE_APPLICATION_KEY clientKey:PARSE_CLIENT_KEY];
+ 
+    //Sample testing for Parse connection before registration function.
     
-    PFUser *user = [PFUser user];
-    user.username = @"user@user.com";
-    user.password = @"123456";
-    user.email = @"user@user.com";
+//    PFUser *user = [PFUser user];
+//    user.username = @"user@user.com";
+//    user.password = @"123456";
+//    user.email = @"user@user.com";
     
-    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
-        if (succeeded) {
-            NSLog(@"Registration succeeded!");
-        }else{
-            NSLog(@"Error on registration: %@", error.localizedDescription);
-        }
-    }];
+//    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+//        if (succeeded) {
+//            NSLog(@"Registration succeeded!");
+//        }else{
+//            NSLog(@"Error on registration: %@", error.localizedDescription);
+//        }
+//    }];
     
     return YES;
 }

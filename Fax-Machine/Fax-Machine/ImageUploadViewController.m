17cce544b955c0d6d8414fd86b8a75034da87bcc
kevinlin505 <kevinlin505@hotmail.com>
2015-11-22 20:32:07 -0500

    Organizing side menu files

--- Fax-Machine/Fax-Machine/ImageUploadViewController.m
+++ Fax-Machine/Fax-Machine/ImageUploadViewController.m
@@ -144,13 +144,6 @@
     [self presentViewController:self.sourcePicker animated:YES completion:nil];
 }
 
-
--(void)pickImageToUpload
-{
-  
-}
-
-
 -(void)upload:(AWSS3TransferManagerUploadRequest*)uploadRequest
 {
   AWSS3TransferManager *transferManager = [AWSS3TransferManager defaultS3TransferManager];
